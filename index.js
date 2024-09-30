e792f252070d152f236d45945baa921d4f9aed4c
Addy Rodriguez <rdz.33.a@gmail.com>
2024-09-30 12:49:13 -0500

    node.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http')
+
+const hostname = '127.0.0.1';
+const PORT = 3000;
+
+// const server = http.createServer((req,res) => {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.end('Hello World')
+// })
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1 style = "color: red"> Hello World! </h1>');
+    res.write('<p> I wonder what else we can send...</p>')
+    res.end();
+})
+
+server.listen(PORT, hostname, () =>{
+    console.log(`Server is running on http://${hostname}:${PORT}`);
+    
+})
